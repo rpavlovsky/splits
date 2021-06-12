bdeb7807db2d7390772af246f85f09dbdee423fb
Rich Pavlovsky <rich@eragon.pavlovs.ky>
2021-06-12 10:23:45 -0500

    Initial commit of Mega Man 2 Normal Any speed run splits file.  I've been using the software flitter for a bit so some data but this is first time checking into source contol.

---
+++ megaman/mman2normalany.scm
@@ -0,0 +1,126 @@
+((title "Mega Man 2: The Mystery of Dr. Wily")
+ (category "Any% (Normal)")
+ (attempts  39)
+ (completed 3)
+ (split_names (
+   "Flash Man"
+   "Heat Man"
+   "Air Man"
+   "Crash Man"
+   "Quick Man"
+   "Wood Man"
+   "Metal Man"
+   "Bubble Man"
+   "Dragon" 
+   "Blocks" 
+   "GutsDozer" 
+   "Booby Trap" 
+   "Rematch" 
+   "Alien"))
+
+(world_record (
+  (attempt 0)
+  (splits (
+       ((title "Flash Man") (time 1:39.0) (is_gold false))
+       ((title "Heat Man") (time 4:13.0) (is_gold false))
+       ((title "Air Man") (time 6:37.0) (is_gold false))
+       ((title "Crash Man") (time 8:59.0) (is_gold false))
+       ((title "Quick Man") (time 10:53.0) (is_gold false))
+       ((title "Wood Man") (time 14:23.0) (is_gold false))
+       ((title "Metal Man") (time 12:29.0) (is_gold false))
+       ((title "Bubble Man") (time 16:15.0) (is_gold false))
+       ((title "Dragon") (time 18:31.0) (is_gold false))
+       ((title "Blocks") (time 20:38.0) (is_gold false))
+       ((title "GutsDozer") (time 22:07.0) (is_gold false))
+       ((title "Booby Trap") (time 24:10.0) (is_gold false))
+       ((title "Rematch") (time 25:58.0) (is_gold false))
+       ((title "Alien") (time 26:50) (is_gold false))
+    ))))
+
+(personal_best
+  (
+   (attempt 39)
+   (splits
+     (
+       ((title "Flash Man") (time 1:48.2) (is_gold false))
+       ((title "Heat Man") (time 5:13.2) (is_gold false))
+       ((title "Air Man") (time 8:52.5) (is_gold false))
+       ((title "Crash Man") (time 12:40.4) (is_gold false))
+       ((title "Quick Man") (time 15:26.8) (is_gold false))
+       ((title "Wood Man") (time 18:31.5) (is_gold false))
+       ((title "Metal Man") (time 21:00.6) (is_gold false))
+       ((title "Bubble Man") (time 23:54.2) (is_gold false))
+       ((title "Dragon") (time 26:58.9) (is_gold false))
+       ((title "Blocks") (time 29:33.2) (is_gold false))
+       ((title "GutsDozer") (time 31:10.9) (is_gold false))
+       ((title "Booby Trap") (time 36:47.9) (is_gold false))
+       ((title "Rematch") (time 39:40.4) (is_gold false))
+       ((title "Alien") (time 41:05.2) (is_gold false))
+     ))
+   )
+  )
+
+ (golds (
+   ((title "Flash Man")  (duration 1:48.2))
+   ((title "Heat Man")   (duration 3:25.0))
+   ((title "Air Man")    (duration 3:39.2))
+   ((title "Crash Man")  (duration 3:47.8))
+   ((title "Quick Man")  (duration 2:46.3))
+   ((title "Wood Man")   (duration 3:04.6))
+   ((title "Metal Man")  (duration 2:29.1))
+   ((title "Bubble Man") (duration 2:53.5))
+   ((title "Dragon")     (duration 3:04.7))
+   ((title "Blocks")     (duration 2:34.3))
+   ((title "GutsDozer")     (duration 1:37.6))
+   ((title "Booby Trap")     (duration 5:36.9))
+   ((title "Rematch")     (duration 2:52.5))
+   ((title "Alien")     (duration 1:24.7))))
+ (history (
+   ((attempt 37)
+    (splits (
+      ((title "Flash Man")  (time 1:46.439)  (is_gold false))
+      ((title "Heat Man")   (time 5:44.332)  (is_gold false))
+      ((title "Air Man")    (time 12:28.599) (is_gold false))
+      ((title "Crash Man")  (time 16:10.479) (is_gold false))
+      ((title "Quick Man")  (time 19:03.972) (is_gold false))
+      ((title "Wood Man")   (time 23:07.525) (is_gold false))
+      ((title "Metal Man")  (time 25:43.666) (is_gold false))
+      ((title "Bubble Man") (time 29:39.894) (is_gold false))
+      ((title "Dragon")     (time 32:44.243) (is_gold false))
+      ((title "Blocks")     (time 35:34.506) (is_gold false))
+      ((title "GutsDozer")     (time 37:38.801) (is_gold false))
+      ((title "Booby Trap")     (time 44:16.730) (is_gold false))
+      ((title "Rematch")     (time 52:20.797) (is_gold false))
+      ((title "Alien")     (time 57:32.529) (is_gold false)))))
+   ((attempt 29)
+    (splits (
+      ((title "Flash Man")  (time 1:44.463)  (is_gold false))
+      ((title "Heat Man")   (time 5:29.414)  (is_gold false))
+      ((title "Air Man")    (time 8:52.245)  (is_gold false))
+      ((title "Crash Man")  (time 12:57.732) (is_gold false))
+      ((title "Quick Man")  (time 16:31.258) (is_gold false))
+      ((title "Wood Man")   (time 22:23.906) (is_gold false))
+      ((title "Metal Man")  (time 25:14.805) (is_gold false))
+      ((title "Bubble Man") (time 28:12.496) (is_gold false))
+      ((title "Dragon")     (time 31:21.332) (is_gold false))
+      ((title "Blocks")     (time 34:03.089) (is_gold false))
+      ((title "GutsDozer")     (time 37:00.407) (is_gold false))
+      ((title "Booby Trap")     (time 47:56.006) (is_gold false))
+      ((title "Rematch")     (time 51:04.775) (is_gold false))
+      ((title "Alien")     (time 52:43.371) (is_gold false)))))
+   ((attempt 22)
+    (splits (
+      ((title "Flash Man")(time 1:55.0) (is_gold false))
+      ((title "Heat Man") (time 5:50.0) (is_gold false))
+      ((title "Air Man")    (time 8:57.0)   (is_gold false))
+      ((title "Crash Man")  (time 13:01.0)   (is_gold false))
+      ((title "Quick Man")  (time 16:55.0)   (is_gold false))
+      ((title "Wood Man")   (time 22:23.0)   (is_gold false))
+      ((title "Metal Man")  (time 25:15.1)   (is_gold false))
+      ((title "Bubble Man") (time 28:30.0)   (is_gold false))
+      ((title "Dragon")     (time 31:25.0)   (is_gold false))
+      ((title "Blocks")     (time 34:15.0)   (is_gold false))
+      ((title "GutsDozer")     (time 37:01.0)   (is_gold false))
+      ((title "Booby Trap")     (time 47:58.0)   (is_gold false))
+      ((title "Rematch")     (time 51:05.0) (is_gold false))
+      ((title "Alien")     (time 53:45.1) (is_gold false))))))))
